57e37403d71e1f9b8b984ef6a100e12f877cd990
Victor Martins <victorcriciuma@gmail.com>
2026-03-01 17:28:27 -0300

    feat: add fastfy api

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log("hello world");
-const name =""
+import "dotenv/config";
+import Fastify from "fastify";
+const fastify = Fastify({
+  logger: true,
+});
+
+fastify.get("/", async function handler() {
+  return { hello: "world" };
+});
+
+try {
+  await fastify.listen({ port: Number(process.env.PORT) ?? 3000 });
+} catch (err) {
+  fastify.log.error(err);
+  fastify.log.error(err);
+  process.exit(1);
+}
